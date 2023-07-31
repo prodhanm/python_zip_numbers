035524d29dbaa541c9b6effd5f7e66b85ab8385a
prodhanm <mustafizur.prodhan@gmail.com>
2023-07-31 16:55:44 -0400

    have an issue with the last index and still trying to solve the problem.

--- zip_num.py
+++ zip_num.py
@@ -5,7 +5,7 @@ num_li_2 = [2,4,6]
 
 def zus_num(num_li_1,num_li_2):
     for num1,num2 in zip(num_li_1,num_li_2):
-        while num2 < 15:
+        while num2 < 15 and num2 != 14:
             if num2%2 == 0:
                 num2 += 2
                 num_li_2.append(num2)
