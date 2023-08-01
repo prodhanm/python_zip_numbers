9e6228e74fde57b2b8f81bf014544fae3487cdee
prodhanm <mustafizur.prodhan@gmail.com>
2023-08-01 13:51:15 -0400

    Added a debugger to solve my problem to the issue and moved it to the .gitignore file.

--- zip_num.py
+++ zip_num.py
@@ -5,13 +5,13 @@ num_li_2 = [2,4,6]
 
 def zus_num(num_li_1,num_li_2):
     for num1,num2 in zip(num_li_1,num_li_2):
-        while num2 < 15 and num2 != 14:
+        while num2 < 12:
             if num2%2 == 0:
                 num2 += 2
                 num_li_2.append(num2)
         u_name = num_li_1 + num_li_2
         s_name = list(set(u_name))
-        return s_name
+    return s_name[0:14]
 
 def main():
     print(zus_num(num_li_1,num_li_2))
